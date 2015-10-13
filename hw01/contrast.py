3e35640c494fb3720bb9c69eece8da1b9f57f6ba
Eugene Auduchinok <eugene.auduchinok@gmail.com>
2015-10-13 16:35:26 +0300

    Add increase contrast

---
+++ hw01/contrast.py
@@ -0,0 +1,21 @@
+# Eugene Auduchinok, 2015
+
+import cv2
+
+img = cv2.imread('low-contrast.png', 0)
+rows, cols = img.shape
+
+min, max, _, _ = cv2.minMaxLoc(img)
+diff = max - min
+multiplier = 255 / diff
+
+for i in range(rows):
+    for j in range(cols):
+        img.itemset((i, j), (img.item(i, j) - min) * multiplier)
+
+cv2.imshow('increase-contrast', img)
+cv2.waitKey(0)
+
+# Do I need to use this method?
+# Windows are closed automatically on OSX + IntelliJ
+# cv2.destroyAllWindows()
